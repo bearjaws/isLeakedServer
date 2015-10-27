5a162ff7cb6b41f9e184569a971ab8754edc4b57
Michael Barajas <michael.a.barajas@gmail.com>
2015-10-27 10:43:39 -0400

    initial commit with krakenjs

---
+++ README.md
@@ -0,0 +1,4 @@
+isLeakedServer
+===========
+
+API server for isLeaked client, used for hosting the isLeaked password list
---
+++ models/password.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function PasswordModel() {
+    this.name = 'password';
+}
+
+PasswordModel.prototype.create = function(knex, body) {
+    
+};
+
+module.exports = PasswordModel;
